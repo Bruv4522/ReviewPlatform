8bce3d3f04ad7a84517ebe8d24fbedfeca62d79f
BluesySugar4522real <bluesysugar4522@gmail.com>
2026-09-10 23:42:12 +0530

    Created TokenRepo.java

---
+++ src/main/java/com/example/ReviewPlatform/Repos/TokenRepo.java
@@ -0,0 +1,7 @@
+package com.example.ReviewPlatform.Repos;
+
+import com.example.ReviewPlatform.Models.SessionToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TokenRepo extends JpaRepository<SessionToken, Long> {
+}
