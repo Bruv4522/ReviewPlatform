a72aa5723bc8b64728c05e6a91c224eb8e7fa9a8
BluesySugar4522real <bluesysugar4522@gmail.com>
2026-09-10 23:29:25 +0530

    Removed a random UUID import in AdminService.java

--- src/main/java/com/example/ReviewPlatform/Services/AdminService.java
+++ src/main/java/com/example/ReviewPlatform/Services/AdminService.java
@@ -5,8 +5,6 @@ import com.example.ReviewPlatform.Repos.AdminRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
 @RequiredArgsConstructor
 public class AdminService {
