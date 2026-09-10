7c3bbc44c5a3265168847abc638e1ed3cf95f41a
BluesySugar4522real <bluesysugar4522@gmail.com>
2026-09-10 16:58:00 +0530

    Initial commit

---
+++ src/test/java/com/example/ReviewPlatform/ReviewPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReviewPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
